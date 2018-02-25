b906d72615c1cc5dd68e9efc05c08d131d95a735
Mark Babcock <mbabcock@gmail.com>
2018-02-25 09:31:36 -0800

    Initial Repo

---
+++ FRC3575/src/org/usfirst/frc/team3575/robot/OI.java
@@ -0,0 +1,28 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team3575.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+
+	 public static Joystick myJoystick = new Joystick(RobotMap.JOYSTICK_PORT);
+	 private static Button elevatorUpButton = new JoystickButton(myJoystick, 1);
+	 
+	 public OI() {
+		 // Bind all of the buttons to commands
+		 // elevatorUpButton.whenPressed(command);
+	 }
+
+}
---
+++ FRC3575/src/org/usfirst/frc/team3575/robot/RobotMap.java
@@ -0,0 +1,24 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team3575.robot;
+
+public class RobotMap {
+	
+	// Drive System Ports
+	public static int LEFT_MOTOR_PORT = 0;
+	public static int RIGHT_MOTOR_PORT = 1;
+	
+	// Climber System Port
+	public static int CLIMBER_PORT = 2;
+	
+	// Loader System Port
+	public static int LOADER_PORT = 3;
+	
+	// JoyStick Ports
+	public static int JOYSTICK_PORT = 0;
+}
